64bba2a1a56784bf8bf06959eeb018bd3b648040
Fredrik Forsmo <frozzare@users.noreply.github.com>
2017-04-03 11:39:22 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -42,10 +42,11 @@ function activate(context) {
             
             editor.edit(function (editorBuilder) {
                 const document = editor.document;
-			    const lastLine = document.lineAt(document.lineCount - 2);
+		const lastLine = document.lineAt(document.lineCount - 2);
 
-			    const start = new vscode.Position(0, 0);
-			    const end = new vscode.Position(document.lineCount - 1, lastLine.text.length);
+                const start = new vscode.Position(0, 0);
+                const end = new vscode.Position(document.lineCount - 1, lastLine.text.length);
+                
                 editorBuilder.replace(new vscode.Range(start, end), data);
             }, {
                 undoStopBefore: true,
